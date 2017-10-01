ff54392c60f5c93da7db81295dbd5ac7ca3f9621
Rocky Wei <finscn@gmail.com>
2017-10-02 01:19:00 +0800

    remove argument `useSquare`

--- src/extension/pixi-tilemap.js
+++ src/extension/pixi-tilemap.js
@@ -46,7 +46,8 @@ var Tiled = Tiled || {};
     };
 
     Map.prototype.createTilemap = function(tileTextures, texPerChild) {
-        var tilemap = new PIXI.tilemap.CompositeRectTileLayer(0, tileTextures, false, texPerChild);
+        // var tilemap = new PIXI.tilemap.CompositeRectTileLayer(0, tileTextures, false, texPerChild);
+        var tilemap = new PIXI.tilemap.CompositeRectTileLayer(0, tileTextures, texPerChild);
 
         return tilemap;
     };
